25fc18d3ca6340a8afaebf14b2039b6c27d71114
Rajat Thomas <rajat@rajat-thomas.net>
2018-03-13 00:24:34 +0100

    improved clarity

--- bandit_epsilon_greedy.py
+++ bandit_epsilon_greedy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class bandit():
@@ -10,5 +11,35 @@ class bandit():
         self.N = 0
 
     def pull(self):
+        return np.random.rand() + self.m
 
-        
+    def update(self, x):
+        self.N += 1
+        self.mean = (1.0 - 1.0/self.N)*self.mean + x/self.N
+
+
+def bandit_experiment(m1, m2, m3, N, eps):
+
+    bandits = [bandit(m1), bandit(m2), bandit(m3)]
+
+    cumulative_mean = []
+    for i in range(N):
+        p = np.random.random()
+        if p < eps:
+            j = np.random.choice(3)
+        else:
+            j = np.argmax([b.mean for b in bandits])
+
+        x = bandits[j].pull()
+        cumulative_mean.append(bandits[j].update(x))
+
+    return cumulative_mean
+
+
+if __name__ == "__main__":
+
+    m1, m2, m3 = (1, 2, 3)
+    N = 10000
+    c_p01 = bandit_experiment(m1, m2, m3, N, 0.01)
+    plt.plot(range(N), c_p01)
+    plt.show()
